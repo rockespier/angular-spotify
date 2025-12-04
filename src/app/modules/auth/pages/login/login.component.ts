4bf3096bec6aa0eb0e964fd28a7c4168714d5ef6
robertoramosdertec <roberto.ramos@dertecglobal.com>
2025-12-04 09:58:50 +0100

    feat: agregar validaciones al formulario de login

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup,ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup,ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +14,10 @@ formLogin: FormGroup = new FormGroup({});
   ngOnInit(): void {
     this.formLogin = new FormGroup({
       // Define your form controls here
-      email: new FormControl(''),
+      email: new FormControl('',[
+        Validators.required,
+        Validators.email
+      ]),
       password: new FormControl('') 
     });
   }
